b8bfe681b4af67c36032b79477dc71ce487f6dba
Mir Saif Ali <mir.saif.ali2004@gmail.com>
2024-09-24 12:08:53 -0700

    URL update

--- Frontend/src/Context/AuthProvider.jsx
+++ Frontend/src/Context/AuthProvider.jsx
@@ -64,7 +64,7 @@ const AuthProvider = ({ children }) => {
       setUser(currentUser);
       if (currentUser) {
         const userInfo = { email: currentUser.email };
-        axios.post("http://localhost:6001/jwt", userInfo).then((response) => {
+        axios.post("https://foodi-cn26.onrender.com/jwt", userInfo).then((response) => {
           if(response.data.token){
             localStorage.setItem("access-token",response.data.token )
           }
--- Frontend/src/Hooks/useAxiosSecure.jsx
+++ Frontend/src/Hooks/useAxiosSecure.jsx
@@ -4,7 +4,7 @@ import useAuth from "./useAuth";
 
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:6001'
+    baseURL: 'https://foodi-cn26.onrender.com'
   });
 
 
